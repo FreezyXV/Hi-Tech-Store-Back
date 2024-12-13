23332961e9cf3b846a8b5644b2321f82e54f6c12
FreezyXV <i_841@mail.ru>
2024-12-13 23:07:53 +0100

    Redis Commented

--- src/utils/redisClient.js
+++ src/utils/redisClient.js
@@ -1,21 +1,21 @@
-const Redis = require("ioredis");
+// const Redis = require("ioredis");
 
-const redisClient = new Redis({
-  host: process.env.REDIS_HOST || "127.0.0.1",
-  port: process.env.REDIS_PORT || 6379,
-  retryStrategy(times) {
-    const delay = Math.min(times * 50, 2000);
-    console.warn(`Retrying Redis connection in ${delay}ms`);
-    return delay;
-  },
-});
+// const redisClient = new Redis({
+//   host: process.env.REDIS_HOST || "127.0.0.1",
+//   port: process.env.REDIS_PORT || 6379,
+//   retryStrategy(times) {
+//     const delay = Math.min(times * 50, 2000);
+//     console.warn(`Retrying Redis connection in ${delay}ms`);
+//     return delay;
+//   },
+// });
 
-redisClient.on("connect", () => {
-  console.log("Connected to Redis");
-});
+// redisClient.on("connect", () => {
+//   console.log("Connected to Redis");
+// });
 
-redisClient.on("error", (err) => {
-  console.error("Redis error:", err.message);
-});
+// redisClient.on("error", (err) => {
+//   console.error("Redis error:", err.message);
+// });
 
-module.exports = redisClient;
+// module.exports = redisClient;
