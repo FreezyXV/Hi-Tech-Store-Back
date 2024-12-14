2c9e8c021bc448e8050106265b6e0a61065b8c16
FreezyXV <i_841@mail.ru>
2024-12-14 10:39:33 +0100

    CORS Options update

--- src/server.js
+++ src/server.js
@@ -41,7 +41,13 @@ const allowedOrigins = [
 ];
 
 const corsOptions = {
-  origin: allowedOrigins,
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
