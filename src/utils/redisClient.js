457056bb8f7fa1c4dd71e5c0928972705b6a8020
FreezyXV <i_841@mail.ru>
2025-04-28 10:56:59 +0200

    Fix redisClient

--- src/utils/redisClient.js
+++ src/utils/redisClient.js
@@ -1,7 +1,11 @@
 // utils/redisClient.js
 const Redis = require("ioredis");
 
-const redisClient = new Redis(process.env.REDIS_URL);
+const redisClient = new Redis(process.env.REDIS_URL, {
+  tls: {
+    rejectUnauthorized: false, // Important for Upstash / Vercel
+  },
+});
 
 redisClient.on("connect", () => {
   console.log("✅ Connected to Redis (Upstash) successfully!");
